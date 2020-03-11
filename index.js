9e36436476d3626c4de55b0a780dda9971ef10c7
guiruffo <guiruffoo@gmail.com>
2020-03-11 12:11:29 +0000

    commit

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
 const express = require('express');
 const server = express();
 var objetos = [
-    {id: 1, descricao: 'Comprar pão', finalizado: false},
+    {
+        id: 1,
+        descricao: 'Comprar pão',
+        finalizado: false
+    },
 ];
-
 server.use(express.json());
 server.get('/objeto', function(request, response){
     return response.json(objetos);
-})
+});
 
 server.get('/objeto/:id', function(request, response){
     const id = request.params.id;
     const objeto = objetos.filter(o => o.id == id);
     return response.json(objeto);
-})
-
+});
 server.post('/objeto', function(request, response){
     const objeto = request.body;
     objetos.push(objeto);
     return response.status(201).send();
-})
+});
+
 server.delete('/objeto/:id', function(request, response){
     const id = request.params.id;
     objetos = objetos.filter(o => o.id != id);
     return response.status(200).send();
-})
+});
 
 server.put('/objeto/:id', function(request, response){
     const id = request.params.id;
@@ -34,7 +37,7 @@ server.put('/objeto/:id', function(request, response){
         if(o.id == id) {
             o.descricao = objeto.descricao;
             o.finalizado = objeto.finalizado;
-            return;
+            return
         }
     })
     return response.send();
