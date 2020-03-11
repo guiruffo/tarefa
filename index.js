c34482e6f6c1f4977ae90f54e55de9a8035ae5fd
guiruffo <guiruffoo@gmail.com>
2020-03-11 00:47:10 +0000

    commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const server = express();
 var objetos = [
     {id: 1, descricao: 'Comprar pão', finalizado: false},
+    {id: 2, descricao: 'Comprar leite', finalizado: true},
 ];
 
 server.use(express.json());
